7c1b4e0ccb6bf3ffaa2aec3035a3afb88e9b9295
bushh3 <123814083+bushh3@users.noreply.github.com>
2025-02-24 08:47:16 +0000

    Removed Unneeded Files

--- quiz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/questions/', views.fetch_questions, name="fetch_questions"),  # get question
-    path('api/check-answer/', views.check_answer, name="check_answer"),  # check answer
-    path('api/quiz-results/', views.get_quiz_results, name="get_quiz_results"),  # get result
-]
