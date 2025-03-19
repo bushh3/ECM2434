485f402b778d447de85fe4919b93f760df161172
bushh3 <123814083+bushh3@users.noreply.github.com>
2025-03-19 10:37:15 +0000

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include  # 导入 include 来包含 quiz 应用的
 from django.shortcuts import redirect
 from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from .forms import EmailBasedPasswordResetForm
+from .views import CustomPasswordResetView, CustomPasswordResetConfirmView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from . import views
 
 app_name = "core"
@@ -15,9 +20,20 @@ urlpatterns = [
     path('questions/', views.fetch_questions, name="fetch_questions"),  # get question
     path('check-answer/', views.check_answer, name="check_answer"),  # check answer
     path('quiz-results/', views.get_quiz_results, name="get_quiz_results"),
-    path('profile/', views.profile_view, name="profile"),
     path('admin/', admin.site.urls),
+    path('profile/', views.profile_view, name="profile"),
+    path('api/user/avatar', views.upload_avatar, name="upload_avatar"),
+    path('api/user/profile', views.get_user_profile, name="get_user_profile"),
+    path('api/user/get-avatar', views.get_avatar, name="get_avatar"),
+    path('api/user/delete', views.delete_account, name="delete_account"),
+    path('api/user/update', views.update_user_profile, name="update_user_profile"),
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name='registration/password_change_form.html', success_url='/accounts/password_change/done/'), name='password_change'),
+    path('accounts/password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    path('api/logout', views.logout_view, name="logout"),
     path('walkinggame/', views.walking_game, name="walkinggame"),
     path('walkinggame/save/', views.save_trip, name="save_trip"),
     path('walkinggame/history/', views.get_trip_history, name="get_trip_history"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
