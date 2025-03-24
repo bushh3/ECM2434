8bf599f09493ef2770cbe35f72357b035c1c35d6
wwx7718 <wwx7718@163.com>
2025-03-24 20:51:40 +0800

    Update

--- GreenQuest/settings.py
+++ GreenQuest/settings.py
@@ -20,9 +20,9 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'django-insecure-w6b11v5ych#3)9t+%w)6=g@^8g57^wet-ggctyeysxo=9q-6_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greenquest-ge4y.onrender.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,6 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'corsheaders.middleware.CorsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'GreenQuest.urls'
 
